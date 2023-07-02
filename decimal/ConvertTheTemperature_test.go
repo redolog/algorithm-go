ef1f46cc02c6e0ae6f15c450bd6505ab7190568c
dragonsong <songhuilong1024@outlook.com>
2023-07-02 10:46:47 +0800

    feat: Add ConvertTheTemperature + FindThePivotInteger with UT;

---
+++ decimal/ConvertTheTemperature_test.go
@@ -0,0 +1,39 @@
+package decimal
+
+import (
+	"algorithm-go/util"
+	"testing"
+)
+
+func Test_convertTemperature(t *testing.T) {
+	type args struct {
+		celsius float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want []float64
+	}{
+		{
+			name: "test1",
+			args: args{
+				celsius: 36.50,
+			},
+			want: []float64{309.65000, 97.70000},
+		},
+		{
+			name: "test2",
+			args: args{
+				celsius: 122.11,
+			},
+			want: []float64{395.26000, 251.79800},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := convertTemperature(tt.args.celsius); !util.DeepEquals(got, tt.want, 1e-5) {
+				t.Errorf("convertTemperature() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
